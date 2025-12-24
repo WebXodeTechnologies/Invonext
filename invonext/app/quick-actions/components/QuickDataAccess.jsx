e9b81de34365a343726622aee5bbeff839cf69ce
ak220193 <mail2meak22frcrio@gmail.com>
2025-12-24 18:35:12 +0530

    Changes in the folder structure and added more updates

--- invonext/app/quick-actions/components/QuickDataAccess.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const QuickDataAccess = () => {
-  return (
-    <div>QuickDataAccess</div>
-  )
-}
-
-export default QuickDataAccess
--- invonext/app/quick-actions/components/QuickExport.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const QuickExport = () => {
-  return (
-    <div>QuickExport</div>
-  )
-}
-
-export default QuickExport
